74b2092bee97651892b07aa46c9854edb2631b21
laurencary <armstronglg720@gmail.com>
2023-05-16 13:59:07 -0700

    Add uniq and twoSum

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -1,1 +1,31 @@
 // Your code here
+Array.prototype.uniq = function() {
+    const uniq_eles = []
+    for (let i = 0; i < this.length; i++) {
+        ele = this[i];
+        prev_ele = this[i-1];
+        if (uniq_eles.includes(ele)) {
+            continue;
+        } 
+        uniq_eles.push(ele);
+    }
+    return uniq_eles;
+}
+
+// console.log([1,1,2,3,4].uniq());
+
+Array.prototype.twoSum = function() {
+    const pairs = [];
+    for (let i = 0; i < this.length - 1; i++) {
+        ele1 = this[i];
+        for (let j = i + 1; j < this.length; j++) {
+            ele2 = this[j];
+            if (ele1 + ele2 === 0) {
+                pairs.push([i, j])
+            }
+        }
+    }
+
+    return pairs;
+}
+console.log([-1, 0, 2, -2, 1].twoSum());
