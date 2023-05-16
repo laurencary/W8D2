3c409b6535af83481714b5999d5ccb1a9702152e
hannadarwish <hannadarwish@gmail.com>
2023-05-16 14:39:42 -0700

    phase 1 and 2

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -28,4 +28,20 @@ Array.prototype.twoSum = function() {
 
     return pairs;
 }
-console.log([-1, 0, 2, -2, 1].twoSum());
+// console.log([-1, 0, 2, -2, 1].twoSum());
+
+
+Array.prototype.transpose = function() {
+    const transposed = [];
+    for (let row = 0; row < this.length; row ++) {
+        subArr = [];
+        for(let col = 0; col < this.length; col ++) {
+            subArr.push(this[col][row]);
+        }
+        transposed.push(subArr);
+    }
+    return transposed;
+}
+
+// console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
+
--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -1,1 +1,24 @@
-// Your code here
+
+Array.prototype.myEach = function(callback) {
+    for (let i = 0; i < this.length; i ++) {
+        callback(this[i]);
+    }
+}
+
+function callback(ele) {
+    return ele * 2;
+}
+
+// console.log([2,3,4].myEach(callback));
+
+Array.prototype.myMap = function(cb) {
+    let result = [];
+    this.myEach(function(ele) {
+        result.push(cb(ele))
+    });
+    return result;
+}
+
+// console.log([2,3,4].myMap(callback));
+
+
